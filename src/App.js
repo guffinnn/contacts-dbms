c47f11843caab41a5e4561c9239dfc785c875915
Danik <dashkevich_daniil2006@mail.ru>
2024-01-19 15:41:41 +0300

    added firebase to project and github pages deploy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import HomePage from "./pages/home/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+      <Router>
+          <Routes>
+              <Route path="/contacts-dbms" element={<HomePage />} />
+          </Routes>
+      </Router>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import {initializeApp} from 'firebase/app';
+import { getAnalytics } from "firebase/analytics";
+import {getAuth} from 'firebase/auth';
+import {getFirestore} from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBeqFAHvPqlEfBpM06Dzz2uxcHkIC6gvi4",
+    authDomain: "contacts-dbms.firebaseapp.com",
+    projectId: "contacts-dbms",
+    storageBucket: "contacts-dbms.appspot.com",
+    messagingSenderId: "628752789938",
+    appId: "1:628752789938:web:ab2a97e7f2e6beeb5db4ab",
+    measurementId: "G-1NNQZRVS86"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { analytics, auth, db };
